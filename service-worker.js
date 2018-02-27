bea4eedb581869a6de7580132f126279956ef2aa
ff05 <frank.filius@gmail.com>
2018-02-27 12:42:02 +0100

    Remove fetch method from service worker

--- service-worker.js
+++ service-worker.js
@@ -14,33 +14,3 @@ self.addEventListener('install', function(event) {
     })
   );
 });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      if (response) {
-        return response;
-      }
-
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(
-        function(response) {
-          if(!response || response.state !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          var responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        }
-      );
-    })
-  );
-})
